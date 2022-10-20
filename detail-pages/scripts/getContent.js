9a40c4c453e919673a7a2b3f72ee7330555bf865
V123in4 <vinconloren@gmail.com>
2022-10-20 13:34:46 +0700

    added structure for useable elements

--- detail-pages/scripts/getContent.js
+++ detail-pages/scripts/getContent.js
@@ -1,5 +1,12 @@
 const url = (k) = `url${k}`;
-const key = document.getElementById("key");
+const keyAPI = () => ``;
+const keyDocument = document.getElementById("key");
+const elemImage = document.getElementById("image");
+const elemImageCaption = document.getElementById("image-caption");
+const elemTitle = document.getElementById("title");
+const elemTitleDesc = document.getElementById("title-desc");
+const elemContentTitle = document.getElementById("content-title");
+const elemContentDesc = document.getElementById("content-desc");
 
 // fetch()
 // .then(() => {
