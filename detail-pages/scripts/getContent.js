01e9f7a1d32fd07299c7c9d41ffd387fca4a8f2d
V123in4 <vinconloren@gmail.com>
2022-10-21 23:15:19 +0700

    added capture logic

--- detail-pages/scripts/getContent.js
+++ detail-pages/scripts/getContent.js
@@ -1,17 +1,27 @@
-const url = (k) = `url${k}`;
-const keyAPI = () => ``;
-const keyDocument = document.getElementById("key");
+const param = new URLSearchParams(window.location.search);
+const id = param.get('id');
+
+const url = (id) = `https://634811ba0484786c6e90f2f2.mockapi.io/jobs/${id}`;
+
 const elemImage = document.getElementById("image");
-const elemImageCaption = document.getElementById("image-caption");
 const elemTitle = document.getElementById("title");
 const elemTitleDesc = document.getElementById("title-desc");
 const elemContentTitle = document.getElementById("content-title");
 const elemContentDesc = document.getElementById("content-desc");
 
-// fetch()
-// .then(() => {
+const imageUrl = (dir) => `./images/${dir}`;
 
-// })
-// .catch(() => {
+fetch(url(id))
+.then((result) => {
+  elemImage.setAttribute("src", imageUrl(result.image));
+  elemTitle.textContent = result.title;
+  elemTitleDesc.innerHTML = result.titleDesc;
+  elemContentTitle.textContent = result.elemContentTitle;
+  elemContentDesc.innerHTML = result.elemContentDesc;
 
-// });
+  console.log("content loaded successfully");
+})
+.catch((error) => {
+  elemTitle.innerText = "Error";
+  console.log(error);
+});
