0ae40bdca86a1800c23afa33677fd3c11328c3eb
V123in4 <vinconloren@gmail.com>
2022-10-21 23:36:46 +0700

    integrate with api

--- detail-pages/scripts/getContent.js
+++ detail-pages/scripts/getContent.js
@@ -1,7 +1,7 @@
 const param = new URLSearchParams(window.location.search);
 const id = param.get('id');
 
-const url = (id) = `https://634811ba0484786c6e90f2f2.mockapi.io/jobs/${id}`;
+const url = (id) => `https://634811ba0484786c6e90f2f2.mockapi.io/jobs/${id}`;
 
 const elemImage = document.getElementById("image");
 const elemTitle = document.getElementById("title");
@@ -12,12 +12,17 @@ const elemContentDesc = document.getElementById("content-desc");
 const imageUrl = (dir) => `./images/${dir}`;
 
 fetch(url(id))
-.then((result) => {
-  elemImage.setAttribute("src", imageUrl(result.image));
-  elemTitle.textContent = result.title;
-  elemTitleDesc.innerHTML = result.titleDesc;
-  elemContentTitle.textContent = result.elemContentTitle;
-  elemContentDesc.innerHTML = result.elemContentDesc;
+.then(async (result) => {
+  const data = await result.json();
+
+  console.log(data);
+
+  elemImage.setAttribute("src", toString(imageUrl(data.image)));
+  console.log(data.image);
+  elemTitle.textContent = data.title;
+  elemTitleDesc.innerHTML = data.titleDesc;
+  elemContentTitle.textContent = data.contentTitle;
+  elemContentDesc.innerHTML = data.contentDesc;
 
   console.log("content loaded successfully");
 })
